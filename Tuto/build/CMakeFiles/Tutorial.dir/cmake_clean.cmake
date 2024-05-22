7f61627c5c2678b4b531eb118ce54e15ebd1df46
UcenELma <elma.hocine@gmail.com>
2024-05-22 18:30:05 +0100

    init tuto branch

---
+++ Tuto/build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tuto/build/CMakeFiles/tuto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tuto.dir/tuto.cc.o"
+  "CMakeFiles/tuto.dir/tuto.cc.o.d"
+  "tuto"
+  "tuto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tuto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
