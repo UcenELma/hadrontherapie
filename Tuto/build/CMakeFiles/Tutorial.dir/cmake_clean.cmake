3e1ed9fbb410d67a07bcde52538d7e5c2f807af3
UcenELma <elma.hocine@gmail.com>
2024-05-23 14:04:27 +0100

    emty

--- Tuto/build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Tutorial"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Tuto/build/CMakeFiles/tuto.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tuto.
-# This may be replaced when dependencies are built.
